4f004ddb1ff524d14563812ba171270c9d902ad5
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2023-03-05 23:59:42 +0530

    Update Questions.js

--- src/Components/Questions.js
+++ src/Components/Questions.js
@@ -2,21 +2,22 @@ import React from "react"
 import { MathJax, MathJaxContext } from "better-react-mathjax";
 import "../Components/Questions.css"
 
-export default function Questions({ quesdata, index }) {
+export default function Questions({ quesdata, index,loader,refreshfunc }) {
 
     const config = {
         loader: { load: ["input/asciimath"] }
     };
     return (
         <div className="question_div">
-            <div className="question_number">
+          { loader ? <h2>Loading...</h2> :<> <div className="question_number">
                 <h2>
                     Question No: {index + 1}
                 </h2>
             </div>
                 <MathJaxContext config={config}>
-                    <MathJax>{quesdata[0]?.Question ? quesdata[0]?.Question : "No data"}</MathJax>
+                    <MathJax>{quesdata[0]?.Question ? quesdata[0]?.Question : <button className="refreshbtn" onClick={refreshfunc}>Refresh Page</button>}</MathJax>
                 </MathJaxContext>
+                </>}
         </div>
     )
 }
